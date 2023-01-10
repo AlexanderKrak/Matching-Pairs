aa54366c5b4ebf18f0a701238aa05f4bca755e51
Alexander Krakoczki <alexander.krakoczki@gmail.com>
2023-01-10 18:34:15 +0100

    whole project has added

---
+++ MatchingPairsGames/Form1.cs
@@ -0,0 +1,131 @@
+﻿using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace MatchingPairsGames
+{
+    public partial class Form1 : Form
+    {
+        Label firstClicked = null;
+        Label secondClicked = null;
+
+        Stopwatch startTheGame = new Stopwatch();
+        SoundPlayer test = new SoundPlayer(Properties.Resources.sound1);
+        
+
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+        public Form1()
+        {
+
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            startTheGame.Start();
+
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+            if (clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+                //clickedLabel.ForeColor = Color.Black;
+
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    test.Play();
+                    return;
+                }
+
+                secondClicked = clickedLabel;
+                test.Play();
+                secondClicked.ForeColor = Color.Black;
+
+                GameOver();
+
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
+
+                timer1.Start();
+
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void GameOver()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+            startTheGame.Stop();
+            TimeSpan timeTaken = startTheGame.Elapsed;
+            string foo = timeTaken.ToString(@"m\:ss\.fff");
+
+            MessageBox.Show("You matched all the icons. You have earned a banana. Well Done you prick. \nTime taken: " + foo);
+            Close();
+        }
+
+
+        
+    }
+}
